ac8b8db8c91c2ce8f19a96956ecb74252c72103f
aman-1216 <114939176+aman-1216@users.noreply.github.com>
2022-10-03 21:47:07 +0530

    create strings.cpp

---
+++ create strings.cpp
@@ -0,0 +1,18 @@
+#include <iosterm>
+#include <satring>
+using namespace  std;
+int main() {
+string a,b;
+cin>>a;
+cin>>b;
+int len1= a.size();
+int len2= b.size();
+cout<<len1<<" "<<len2<<" "<<endl;
+cout<<a+b<<endl;
+char temp=a[0];
+a[0]=b[0];
+b[0]=temp;
+cout<<a<<" "<<b<<"";
+
+    return 0;
+}
