80e59e042718a7adecf60d8f55f2d0289d3ef379
aman-1216 <114939176+aman-1216@users.noreply.github.com>
2022-10-03 22:19:20 +0530

    create for loop.cpp

---
+++ create for loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+    // complete the code.
+    int i,m;
+    cin>>i>>m;
+    for (int n=i;n<=m;n++)
+    {
+    if (n==1) 
+    {printf("one");}
+    else if (n==2)
+        {printef("two");}
+    else if (n==3)
+        {printf("three");}
+        else if (n==4)
+            {printf("four");}
+            else if (n==5)
+                {
